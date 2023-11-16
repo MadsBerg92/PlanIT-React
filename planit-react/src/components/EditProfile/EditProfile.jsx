851e33b04a696bbf5b1def266a2709dc9b7083e2
Mads Berg <madbe@itu.dk>
2023-11-16 20:47:21 +0100

    working on implementing the edit user component

--- planit-react/src/components/EditProfile/EditProfile.jsx
+++ planit-react/src/components/EditProfile/EditProfile.jsx
@@ -1,31 +1,81 @@
 // EditProfile.jsx
-import { useNavigate } from "react-router-dom";
-import styles from "../../pages/ProfilePage/ProfilePage.module.css";
 
-const EditProfile = () => {
-  const navigate = useNavigate();
+import styles from "../../pages/ProfilePage/ProfilePage.module.css";
+import { useState, useEffect } from "react";
+import Parse from "parse";
+
+const EditProfile = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [profile, setProfile] = useState(null);
+  useEffect(() => {
+    const user = Parse.User.current();
+    if (user) {
+      setProfile(user);
+    } else {
+      console.log("No user is currently logged in");
+    }
+  }, []);
+
+  if (!profile) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <div className={styles.details}>
-      <h2>My Profile</h2>
-      <p>
-        <strong>Email:</strong>{" "}
-        <input type="email" defaultValue="mads.nielsen@email.dk" />
-      </p>
-      <p>
-        <strong>Phone:</strong>{" "}
-        <input type="tel" defaultValue="+45 1234 5678" />
-      </p>
-      <p>
-        <strong>Birthday:</strong>{" "}
-        <input type="date" defaultValue="1990-04-15" />
-      </p>
-      <button
-        className={styles.saveButton}
-        onClick={() => navigate("/profile")}
-      >
-        Save Changes
-      </button>
+    <div>
+      {isEditing ? (
+        <div>
+          <div className={styles.header}>
+            <h1>My Profile</h1>
+          </div>
+          <div className={styles.editSection}>
+            <form action="/updateProfile" method="post">
+              <input type="file" id="img" name="img" accept="image/*" />
+              <input defaultValue={profile.get("username")} />
+              <input
+                type="email"
+                placeholder="Email"
+                defaultValue={profile.get("email")}
+              />
+
+              <button
+                type="submit"
+                onClick={() => {
+                  setIsEditing(false);
+                }}
+                className={styles.updateButton}
+              >
+                Update
+              </button>
+            </form>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <div className={styles.header}>
+            <h2>Edit Profile</h2>
+          </div>
+          <div className={styles.editSection}>
+            <p>
+              <strong>User Name:</strong>{" "}
+              <input type="email" defaultValue={profile.get("name")} />
+            </p>
+            <p>
+              <strong>Email:</strong>{" "}
+              <input type="name" defaultValue={profile.get("email")} />
+            </p>
+            {/* <p>
+            <strong>Birthday:</strong>{" "}
+            <input type="date" defaultValue="1990-04-15" />
+          </p> */}
+            <button
+              className={styles.saveButton}
+              onClick={() => setIsEditing(true)}
+            >
+              Save Changes
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
