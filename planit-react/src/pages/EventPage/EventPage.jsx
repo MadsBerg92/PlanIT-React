76724f3c7dcc3e3b66771c3d8c7e071c077e87e1
TheoITU <trit@itu.dk>
2023-11-21 16:52:27 +0100

    Toggle buttons og databse justering pis

--- planit-react/src/pages/EventPage/EventPage.jsx
+++ planit-react/src/pages/EventPage/EventPage.jsx
@@ -7,19 +7,44 @@ import { ShoppingListContext } from "../../Context/ShoppingListContext.jsx";
 import React from "react";
 // import ShoppingList from "../../components/ShoppingList/ShoppingList.jsx";
 import Box from "../../components/box/Box.jsx";
-import Parse from "parse";
+import Parse, { User } from "parse";
 
 const EventPage = () => {
   const { eventId } = useParams();
   //useParams returns a string - so we parse it to a number to match the eventId datatype
   const eventIdAsNumber = parseInt(eventId, 10);
+  const [isActive, setIsActive] = useState(false);
   // Data for information box
   const [eventData, setEventData] = useState([]);
   const { shoppingList } = React.useContext(ShoppingListContext);
   const [description, setDescription] = useState("");
+  const userId = Parse.User.current();
 
   const handleButtonClick = () => {
     alert("Button clicked!");
+  };
+
+  const handleToggle = async () => {
+    console.log("Event ID:", eventIdAsNumber);
+    setIsActive(!isActive);
+
+    try {
+      const ParseEvents = Parse.Object.extend("Events");
+      const query = new Parse.Query(ParseEvents);
+
+      // Fetch the specific event using the eventId
+      const event = await query.get("rZksBvJUBT"); // Ensure eventIdAsNumber is a string if it's required by your Parse setup
+      console.log(event);
+      if (!isActive) {
+        event.addUnique("attendees", userId); // Assuming 'userId' is defined
+      } else {
+        event.remove("attendees", userId);
+      }
+
+      await event.save(); // Save the updated event
+    } catch (error) {
+      console.error("Error updating event status:", error);
+    }
   };
 
   useEffect(() => {
@@ -27,18 +52,14 @@ const EventPage = () => {
       try {
         const ParseEvents = Parse.Object.extend("Events");
         const query = new Parse.Query(ParseEvents);
-        console.log(eventId);
         query.equalTo("eventId", eventIdAsNumber);
         query.select(
           "eventLocation",
-          "createdBy",
+          "creatorName",
           "eventDate",
           "eventDescription"
         );
-
-        console.log(query);
         const result = await query.first();
-        console.log(result);
 
         const eventDataFromParse = [
           {
@@ -47,7 +68,7 @@ const EventPage = () => {
           },
           {
             label: "Created By",
-            value: result.get("createdBy"),
+            value: result.get("creatorName"),
           },
           {
             label: "Event Date",
@@ -78,8 +99,10 @@ const EventPage = () => {
         <Button
           textActive={"Attending"}
           textInactive={"Not Attending"}
+          isActive={isActive}
+          onClick={handleToggle}
           type={"normal"}
-        ></Button>
+        />
         <Button
           textInactive={"Invite Friends"}
           type={"special"}
