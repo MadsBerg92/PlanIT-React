b86a797f54dd6eb4bd83748c26b97353a7c50b8d
elsb <elsb>
2023-12-19 11:36:26 +0100

    Eventpage now renders amount of attendees and their names

--- planit-react/src/pages/EventPage/EventPage.jsx
+++ planit-react/src/pages/EventPage/EventPage.jsx
@@ -7,12 +7,20 @@ import FriendListModal from "../../components/FriendListModal/FriendListModal.js
 import styles from "./EventPage.module.css";
 import Parse from "parse";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocationDot, faUser, faCalendarDays } from "@fortawesome/free-solid-svg-icons"; 
+import {
+  faLocationDot,
+  faUser,
+  faCalendarDays,
+  faUsers,
+} from "@fortawesome/free-solid-svg-icons";
 
-//Transfrom fetched date to a more readable format
+// Transfrom fetched date to a more readable format
 const formatDate = (dateString) => {
-  const options = { weekday: 'long', day: 'numeric', month: 'long' };
-  const formattedDate = new Date(dateString).toLocaleDateString('en-US', options);
+  const options = { weekday: "long", day: "numeric", month: "long" };
+  const formattedDate = new Date(dateString).toLocaleDateString(
+    "en-US",
+    options
+  );
   return formattedDate;
 };
 
@@ -26,15 +34,16 @@ const EventPage = () => {
   const [shoppingList, setShoppingList] = useState([]);
   const [showFriendList, setShowFriendList] = useState(false);
   const [eventTitle, setEventTitle] = useState("");
-
   const [attendeesData, setAttendeesData] = useState([]);
+  const [attendees, setAttendees] = useState([]); // Added state for attendees
+  const [attendeesCount, setAttendeesCount] = useState(0);
 
   const fetchAttendeesData = async (attendeeIds) => {
     try {
       const ParseUser = Parse.Object.extend("User");
       const userQuery = new Parse.Query(ParseUser);
       userQuery.containedIn("objectId", attendeeIds);
-      userQuery.select("username"); // Add other user fields you want to display
+      userQuery.select("username");
 
       const users = await userQuery.find();
 
@@ -51,113 +60,127 @@ const EventPage = () => {
     }
   };
 
-  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const ParseEvents = Parse.Object.extend("Events");
+        const query = new Parse.Query(ParseEvents);
+        query.equalTo("eventId", eventIdAsNumber);
+        query.select(
+          "eventLocation",
+          "createdBy",
+          "eventDate",
+          "eventDescription",
+          "creatorName",
+          "image",
+          "shoppingList",
+          "title",
+          "attendees"
+        );
 
+        const result = await query.first();
 
-  const fetchEventData = async () => {
-    try {
-      const ParseEvents = Parse.Object.extend("Events");
-      const query = new Parse.Query(ParseEvents);
-      query.equalTo("eventId", eventIdAsNumber);
-      query.select(
-        "eventLocation",
-        "createdBy",
-        "eventDate",
-        "eventDescription",
-        "creatorName",
-        "image",
-        "shoppingList",
-        "title",
-        "attendees" 
-      );
+        const currentUserId = Parse.User.current().id;
 
-      const result = await query.first();
+        const attendees = result.get("attendees") || [];
+        const attendeesWithoutCurrentUser = attendees.filter(
+          (attendeeId) => attendeeId !== currentUserId
+        );
 
-      // Check if the current user is in the list of attendees
-      const attendees = result.get("attendees") || [];
-      const currentUser = Parse.User.current();
-      const isAttending = attendees.includes(currentUser.id);
+        //showing number of attendees
+        setAttendeesCount(attendeesWithoutCurrentUser.length);
 
-      // Set the initial state based on user's attendance
-      setIsActive(isAttending);
+        const currentUser = Parse.User.current();
+        const isAttending = attendees.includes(currentUser.id);
 
-      setEventTitle(result.get("title"));
+        setIsActive(isAttending);
+        setEventTitle(result.get("title"));
 
-      // Fetch usernames of attendees
-      await fetchAttendeesData(attendees);
+        await fetchAttendeesData(attendeesWithoutCurrentUser);
 
-      
-    console.log("Attendees Data:", attendeesData);
+        const updatedAttendeesData = attendeesData
+          .map((attendee) => attendee.value)
+          .join(", ");
 
-      setEventData([
-        { label: (
-          <>
-            <FontAwesomeIcon icon={faLocationDot} /> Location
-          </>
-        ),
-        value: result.get("eventLocation"),
-      },
-      { label: (
-        <>
-          <FontAwesomeIcon icon={faUser} /> Hosted by
-        </>
-      ), value: result.get("creatorName") },
-      { label: (
-        <>
-          <FontAwesomeIcon icon={faCalendarDays} /> Date
-        </>
-      ), value: formatDate(result.get("eventDate")) },
-      { label: ( <> <FontAwesomeIcon icon={faUser} /> Attendees </> ), value: attendeesData.map((attendee) => attendee.value).join(", ") },
-      ]);
-      const eventImage = result.get("image").url();
-      setEventImage(eventImage);
-      setDescription(result.get("eventDescription"));
-      setShoppingList(result.get("shoppingList"));
-    } catch (error) {
-      console.error("Error fetching event data:", error);
-    }
-  };
+        setEventData([
+          {
+            label: (
+              <>
+                <FontAwesomeIcon icon={faLocationDot} /> Location
+              </>
+            ),
+            value: result.get("eventLocation"),
+          },
+          {
+            label: (
+              <>
+                <FontAwesomeIcon icon={faUser} /> Hosted by
+              </>
+            ),
+            value: result.get("creatorName"),
+          },
+          {
+            label: (
+              <>
+                <FontAwesomeIcon icon={faCalendarDays} /> Date
+              </>
+            ),
+            value: formatDate(result.get("eventDate")),
+          },
+          {
+            label: (
+              <>
+                {" "}
+                <FontAwesomeIcon icon={faUsers} />
+                {` ${attendeesCount} `} People attending{" "}
+              </>
+            ),
+            value: updatedAttendeesData,
+          },
+        ]);
 
+        const eventImage = result.get("image").url();
+        setEventImage(eventImage);
+        setDescription(result.get("eventDescription"));
+        setShoppingList(result.get("shoppingList"));
+        setAttendees(attendeesWithoutCurrentUser); // Set the attendees state
+      } catch (error) {
+        console.error("Error fetching event data:", error);
+      }
+    };
+
+    fetchData();
+  }, [eventIdAsNumber, attendees, attendeesData]);
 
   const handleToggle = async () => {
     try {
       const ParseEvents = Parse.Object.extend("Events");
       const query = new Parse.Query(ParseEvents);
 
-      // Fetch the specific event using the eventId
       query.equalTo("eventId", eventIdAsNumber);
 
-      const event = await query.first(); // Use the dynamic eventId here
+      const event = await query.first();
 
-      // Get the userId
       const userId = Parse.User.current().id;
-
-      // Fetch the attendees array
       const attendees = event.get("attendees") || [];
 
       if (!isActive && !attendees.includes(userId)) {
-        attendees.push(userId); // Add the userId to the attendees array
+        attendees.push(userId);
         setIsActive(true);
       } else {
         const index = attendees.indexOf(userId);
         if (index > -1) {
-          attendees.splice(index, 1); // Remove the userId from the attendees array
+          attendees.splice(index, 1);
         }
         setIsActive(false);
       }
 
-      event.set("attendees", attendees); // Update the attendees array
-
-      await event.save(); // Save the updated event
+      event.set("attendees", attendees);
+      await event.save();
     } catch (error) {
       console.error("Error updating event status:", error);
     }
   };
-
-  useEffect(() => {
-    
-    fetchEventData();
-  }, [eventId, eventIdAsNumber]);
 
   const handleModalOpen = () => {
     setShowFriendList(true);
